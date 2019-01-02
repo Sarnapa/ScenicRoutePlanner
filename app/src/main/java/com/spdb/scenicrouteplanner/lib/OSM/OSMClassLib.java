3d9b2564d6c8d954c295b36a213c71c30992ee75
Sarnapa <piotrak.michal@gmail.com>
2019-01-02 14:32:37 +0100

    Modified WayType

--- app/src/main/java/com/spdb/scenicrouteplanner/lib/OSM/OSMClassLib.java
+++ app/src/main/java/com/spdb/scenicrouteplanner/lib/OSM/OSMClassLib.java
@@ -4,34 +4,22 @@ public final class OSMClassLib
 {
     public enum WayType
     {
-        MOTORWAY("motorway"),
-        TRUNK("trunk"),
-        PRIMARY("primary"),
-        SECONDARY("secondary"),
-        TERTIARY("tertiary"),
-        UNCLASSIFIED("unclassified"),
-        RESIDENTIAL("residential"),
-        SERVICE("service"),
-        UNKNOWN("unknown");
-
-        private final String wayName;
-
-        public String getWayName()
-        {
-            return wayName;
-        }
+        MOTORWAY,
+        TRUNK,
+        PRIMARY,
+        SECONDARY,
+        TERTIARY,
+        UNCLASSIFIED,
+        RESIDENTIAL,
+        SERVICE,
+        UNKNOWN;
 
         public static String getWayTypesListText()
         {
-            return String.format("'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'", MOTORWAY.getWayName(),
-                    TRUNK.getWayName(), PRIMARY.getWayName(), SECONDARY.getWayName(), TERTIARY.getWayName(),
-                    UNCLASSIFIED.getWayName(), RESIDENTIAL.getWayName(), SERVICE.getWayName(),
-                    UNKNOWN.getWayName());
-        }
-
-        WayType(String value)
-        {
-            wayName = value;
+            return String.format("'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'", MOTORWAY.name(),
+                    TRUNK.name(), PRIMARY.name(), SECONDARY.name(), TERTIARY.name(),
+                    UNCLASSIFIED.name(), RESIDENTIAL.name(), SERVICE.name(),
+                    UNKNOWN.name());
         }
     }
 
