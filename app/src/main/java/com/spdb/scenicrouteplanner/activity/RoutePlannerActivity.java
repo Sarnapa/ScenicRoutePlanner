a23b6b1a8d516c3ff2f6bc0c56bb2533a553ed5e
Sarnapa <piotrak.michal@gmail.com>
2019-01-05 13:05:27 +0100

    RoutePlannerActivity merge

--- app/src/main/java/com/spdb/scenicrouteplanner/activity/RoutePlannerActivity.java
+++ app/src/main/java/com/spdb/scenicrouteplanner/activity/RoutePlannerActivity.java
@@ -17,8 +17,6 @@ import com.spdb.scenicrouteplanner.model.Model;
 import com.spdb.scenicrouteplanner.service.OSMService;
 import com.spdb.scenicrouteplanner.utils.OSMParser;
 
-import org.spatialite.database.SQLiteDatabase;
-
 public class RoutePlannerActivity extends Fragment {
     // ==============================
     // Private fields
@@ -30,29 +28,33 @@ public class RoutePlannerActivity extends Fragment {
 
     private OSMService osmService;
 
-    public RoutePlannerActivity() {
+    public RoutePlannerActivity()
+    {
         super();
 
         this.osmService = new OSMService();
     }
 
     @Override
-    public void onCreate(Bundle savedInstance) {
+    public void onCreate(Bundle savedInstance)
+    {
         super.onCreate(savedInstance);
     }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         routePlannerView = inflater.inflate(R.layout.activity_route_planner, container, false);
 
-        if (routePlannerView != null) {
+        if (routePlannerView != null)
+        {
             startLocationEditText = routePlannerView.findViewById(R.id.start_location);
             startLocationEditText.setText("Warszawa, Nowowiejska 30");
             destinationEditText = routePlannerView.findViewById(R.id.destination);
             destinationEditText.setText("Warszawa, Nowogrodzka 70");
             scenicRouteMinEditText = routePlannerView.findViewById(R.id.scenic_route_min_length);
-            scenicRouteMinEditText.setText("100");
+            scenicRouteMinEditText.setHint("Distance in kilometers");
         }
 
         return routePlannerView;
@@ -66,37 +68,53 @@ public class RoutePlannerActivity extends Fragment {
 
         findRoutePlannerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 String start = startLocationEditText.getText().toString();
                 String dest = destinationEditText.getText().toString();
                 String scenicRouteMin = scenicRouteMinEditText.getText().toString();
 
-                if (!(start.isEmpty() || dest.isEmpty() || scenicRouteMin.isEmpty())) {
+                if(!(start.isEmpty() || dest.isEmpty() || scenicRouteMin.isEmpty()))
+                {
                     MapActivity.getMapService().removeAllEdges();
+
+                    RoutesDbProvider dbProvider = MapActivity.getDbProvider();
+                    dbProvider.clearDb();
 
                     GeoCoords startCoords = osmService.getPlaceCoords(start);
                     GeoCoords destCoords = osmService.getPlaceCoords(dest);
-                    Double latDiff = Math.abs(startCoords.getLatitude()-destCoords.getLatitude());
-                    Double lonDiff = Math.abs(startCoords.getLongitude()-destCoords.getLongitude());
-                    if((latDiff > 0.15) || (lonDiff > 0.15)){
+                    Double latDiff = Math.abs(startCoords.getLatitude() - destCoords.getLatitude());
+                    Double lonDiff = Math.abs(startCoords.getLongitude() - destCoords.getLongitude());
+                    if((latDiff > 0.15) || (lonDiff > 0.15))
+                    {
                         //TODO:Alert
-                    } else {
+                    }
+                    else
+                    {
                         osmService.getMapExtent(startCoords, destCoords);
-                        try {
+                        try
+                        {
                             OSMParser parser = new OSMParser();
                             Model model = parser.parseOSMFile(Environment.getExternalStorageDirectory() + "/SRP/maps/osm");
+
+                            dbProvider.addWays(model.getWaysList());
+                            dbProvider.addEdges(model.getEdgesList());
+                            model.setEdges(dbProvider.getAllEdges());
+
                             MapActivity.getMapService().addEdges(model.getEdgesList());
-                        } catch (Exception e) {
+
+                            MapActivity.getMapService().setStartMapExtent(startCoords, destCoords);
+
+                        }
+                        catch (Exception e)
+                        {
                             //TODO:You requested too many nodes (limit is 50000). Either request a smaller area, or use planet.osm
                             e.printStackTrace();
                         }
-                        RoutesDbProvider routesDbProvider = new RoutesDbProvider(getContext());
-                        SQLiteDatabase db = routesDbProvider.getRoutesDb();
-
-                        // Rozwiązanie chwilowe - start i koniec bedzie czytany z bazy
-                        MapActivity.getMapService().setStartMapExtent(startCoords, destCoords);
-
-                        getFragmentManager().popBackStack();
+                        finally
+                        {
+                            getFragmentManager().popBackStack();
+                        }
                     }
                 }
             }
